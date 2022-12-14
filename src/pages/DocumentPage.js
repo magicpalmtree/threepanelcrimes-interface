fadabe12ed3df3af154d1a6f5439090ec25984a4
magicpalmtree <magicpalmtree.space@gmail.com>
2022-12-14 14:58:56 -0500

    Added no styled boring html for document

---
+++ src/pages/DocumentPage.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./bootstrap.min.css";
+
+const Document = () => {
+    return (
+        <div className="container py-20">
+            <strong>threepanelcrimes</strong>
+
+            <p>ART HEIST - GARDNER 1990</p>
+
+            <strong>7th December 2022</strong>
+
+            <h1><strong>OVERVIEW</strong></h1>
+            <p>ART HEIST is an ethereum-based game and interactive art collecting experience.</p>
+
+            <h1><strong>GOALS</strong></h1>
+            <p>1. To have fun by poking fun.</p>
+            <p>2. To bring attention towards the black market for stolen art and tell a good story.</p>
+
+            <h1><strong>SPECIFICATIONS</strong></h1>
+            <p>Each primary collector of a 1/1 panel in the ART HEIST series on SuperRare will have a chance at deciding what happens on the art black market. The collectors may work together or compete for the final say on the fate of the stolen masterpiece.</p>
+
+            <h1><strong>MILESTONES</strong></h1>
+            <h2><strong>AUCTION 1/1 ON SUPERRARE</strong></h2>
+            <p>Each panel was auctioned off on superrare. Each of the three auction winners was airdropped a CANVAS ROLL. Each winner gets to form his own crew, tasked with offloading/selling the painting. Each bidder from each auction received a TOOL by airdrop as a thank you.</p>
+
+            <h2><strong>TICKETS</strong></h2>
+            <p>1.7M plane tickets were minted as an invitation to the auction of the stolen art in NYC. Each ticket is weighted at 1 vote per wallet minted. There were approx. 200 wallets that minted the tickets. (we have fun plans for all those extra tickets don’t worry).</p>
+
+            <h2><strong>UNDERGROUND AUCTION / VOTE</strong></h2>
+            <p>The first of the three crews to burn to redeem their crew token will be crowned the winner. Extra points will be added for TOOL bidder’s editions burned. The winner of the underground auction/vote will decide what happens to the stolen painting from three options:</p>
+            <p>1. The painting is restored to the gallery and offered as a free open edition for 24 hours to the public.</p>
+            <p>2. The painting is not restored to the gallery and an empty frame limited edition will be airdropped to the members of their crew. Additionally an open edition will be minted at .069 ETH for 72 hours with 100% of mint funds sent to the boss of the crew who will disperse them as they see fit.</p>
+            <p>3. The painting is destroyed and a mystery 1/1 piece is minted to the wallet of the winner for their private collection.</p>
+
+            <h2><strong>LINKS:</strong></h2>
+
+            <p>SuperRare: <a href="https://tinyurl.com/threepanelcrimes">https://tinyurl.com/threepanelcrimes</a></p>
+
+            <p>TOOLS & Tickets: <a href="https://opensea.io/collection/artheists">https://opensea.io/collection/artheists</a></p>
+
+            <p>Manifold Burn to redeem voting claims:</p>
+
+            <p>MrKrabs: link soon</p>
+            <p>Thebenmeadows: link soon</p>
+            <p>Tankbottoms: link soon</p>
+
+            <p>Plan for the ART HEIST SITE:</p>
+            
+            <p>ONE PAGE</p>
+            <p>WITH THE VIBES OF THIS:</p>
+            <a href="https://www.berkshirehathaway.com/">https://www.berkshirehathaway.com/</a>
+        </div>
+    )
+}
+
+export default Document;
